5d42eff6dc54c0bf5cdb27cd07ab6fc495d4a4e9
Garland Key <hello@garlandkey.com>
2023-01-16 16:12:30 -0500

    changed graphics to match silver blue color theme. Fixed responsiveness for form.

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-const cards = document.querySelectorAll(".card");
+// const cards = document.querySelectorAll(".card");
 
-cards.forEach( card => {
-    const cardFront = card.children[0];
-    const cardBack = card.children[1];
+// cards.forEach( card => {
+//     const cardFront = card.children[0];
+//     const cardBack = card.children[1];
 
-    card.addEventListener("mouseover", (event) => {
-        event.preventDefault();
-        cardFront.classList.remove("card-front-slide-in");
-        cardBack.classList.remove("card-back-slide-out");
-        cardFront.classList.add("card-front-slide-out");
-        cardBack.classList.add("card-back-slide-in");
-    });
+//     card.addEventListener("mouseover", (event) => {
+//         event.preventDefault();
+//         cardFront.classList.remove("card-front-slide-in");
+//         cardBack.classList.remove("card-back-slide-out");
+//         cardFront.classList.add("card-front-slide-out");
+//         cardBack.classList.add("card-back-slide-in");
+//     });
 
-    card.addEventListener("mouseout", (event) => {
-        event.preventDefault();
-        cardFront.classList.remove("card-front-slide-out");
-        cardBack.classList.remove("card-back-slide-in");
-        cardFront.classList.add("card-front-slide-in");
-        cardBack.classList.add("card-back-slide-out");
-    });
+//     card.addEventListener("mouseout", (event) => {
+//         event.preventDefault();
+//         cardFront.classList.remove("card-front-slide-out");
+//         cardBack.classList.remove("card-back-slide-in");
+//         cardFront.classList.add("card-front-slide-in");
+//         cardBack.classList.add("card-back-slide-out");
+//     });
 
-});
+// });
