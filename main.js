ed73f272b4cb7bbade97e2e8ab8d98ded7c0c1d4
Garland Key <hello@garlandkey.com>
2023-01-17 10:38:07 -0500

    changed sizing of form so that it's a more reasonable size

--- main.js
+++ main.js
@@ -1,55 +1,26 @@
 const cards = document.querySelectorAll(".card");
-const cardBack = document.querySelector(".card-back");
 
 cards.forEach( card => {
     const cardFront = card.children[0];
     const cardBack = card.children[1];
 
-    card.addEventListener("mouseover", (event) => {
+    card.addEventListener("mouseover" || "active", (event) => {
         event.preventDefault();
-
         cardFront.classList.remove("card-front-slide-in");
         cardBack.classList.remove("card-back-slide-out");
         cardFront.classList.add("card-front-slide-out");
         cardBack.classList.add("card-back-slide-in");
-
-        console.log(`mouseover detected\ncard-front class list: ${cardFront.classList}\ncard-back class list: ${cardBack.classList}`);
     });
 
     card.addEventListener("mouseout", (event) => {
         event.preventDefault();
-        
         cardFront.classList.remove("card-front-slide-out");
         cardBack.classList.remove("card-back-slide-in");
         cardFront.classList.add("card-front-slide-in");
         cardBack.classList.add("card-back-slide-out");
-
-        console.log(`mouseout detected\ncard-front class list: ${cardFront.classList}\ncard-back class list: ${cardBack.classList}`);
     });
-
-    card.addEventListener("touch", (event) => {
-        event.preventDefault();
-
-        cardFront.classList.remove("card-front-slide-in");
-        cardBack.classList.remove("card-back-slide-out");
-        cardFront.classList.add("card-front-slide-out");
-        cardBack.classList.add("card-back-slide-in");
-
-        console.log(`mouseover detected\ncard-front class list: ${cardFront.classList}\ncard-back class list: ${cardBack.classList}`);
-    });
-
-    card.addEventListener("mouseout", (event) => {
-        event.preventDefault();
-        
-        cardFront.classList.remove("card-front-slide-out");
-        cardBack.classList.remove("card-back-slide-in");
-        cardFront.classList.add("card-front-slide-in");
-        cardBack.classList.add("card-back-slide-out");
-
-        console.log(`mouseout detected\ncard-front class list: ${cardFront.classList}\ncard-back class list: ${cardBack.classList}`);
-    });
-
 });
 
-let position = document.body.scrollTop
-console.log(position)
+window.addEventListener('scroll', () => {
+    document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+  }, false);
