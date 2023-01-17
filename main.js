90d68fedfff975941a7e49595608a1a656ee1d83
Garland Key <hello@garlandkey.com>
2023-01-16 23:43:14 -0500

    changed shape of cards

--- main.js
+++ main.js
@@ -27,4 +27,29 @@ cards.forEach( card => {
         console.log(`mouseout detected\ncard-front class list: ${cardFront.classList}\ncard-back class list: ${cardBack.classList}`);
     });
 
+    card.addEventListener("touch", (event) => {
+        event.preventDefault();
+
+        cardFront.classList.remove("card-front-slide-in");
+        cardBack.classList.remove("card-back-slide-out");
+        cardFront.classList.add("card-front-slide-out");
+        cardBack.classList.add("card-back-slide-in");
+
+        console.log(`mouseover detected\ncard-front class list: ${cardFront.classList}\ncard-back class list: ${cardBack.classList}`);
+    });
+
+    card.addEventListener("mouseout", (event) => {
+        event.preventDefault();
+        
+        cardFront.classList.remove("card-front-slide-out");
+        cardBack.classList.remove("card-back-slide-in");
+        cardFront.classList.add("card-front-slide-in");
+        cardBack.classList.add("card-back-slide-out");
+
+        console.log(`mouseout detected\ncard-front class list: ${cardFront.classList}\ncard-back class list: ${cardBack.classList}`);
+    });
+
 });
+
+let position = document.body.scrollTop
+console.log(position)
