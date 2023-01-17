d16eadc1bb6c43001b5334cccd0a1809b6a56ef0
Garland Key <hello@garlandkey.com>
2023-01-17 11:49:22 -0500

    added above the fold animations

--- main.js
+++ main.js
@@ -1,26 +1,38 @@
 const cards = document.querySelectorAll(".card");
+let position;
+
+window.addEventListener("scroll", () => {
+    position = window.pageYOffset;
+    console.log("scroll position: " + position + "\n")
+});
+
+const isMobileDevice = () => {
+    return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) ? true : false;
+};
+
+console.log(isMobileDevice());
 
 cards.forEach( card => {
     const cardFront = card.children[0];
     const cardBack = card.children[1];
 
-    card.addEventListener("mouseover" || "active", (event) => {
+    card.addEventListener("mouseover", (event) => {
         event.preventDefault();
+
         cardFront.classList.remove("card-front-slide-in");
         cardBack.classList.remove("card-back-slide-out");
+
         cardFront.classList.add("card-front-slide-out");
         cardBack.classList.add("card-back-slide-in");
     });
 
     card.addEventListener("mouseout", (event) => {
         event.preventDefault();
+        
         cardFront.classList.remove("card-front-slide-out");
         cardBack.classList.remove("card-back-slide-in");
+
         cardFront.classList.add("card-front-slide-in");
         cardBack.classList.add("card-back-slide-out");
     });
 });
-
-window.addEventListener('scroll', () => {
-    document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-  }, false);
